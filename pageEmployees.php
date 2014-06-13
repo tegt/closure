cc72fb3f0548607aca09e4e0dbccbf8ec771c868
tegt <grant@tegt.com>
2014-06-13 09:07:18 -0700

    Save for refactor

---
+++ pageEmployees.php
@@ -0,0 +1,133 @@
+<?php
+/** Paging Ajax responder for Org Chart Viewer
+ *
+ *  Code produced as a quick challenge to show off LAMP stack. This 
+ *  module uses a path table to produce timely paged results for an 
+ *  org chart display of a 20000 node tree.
+ *
+ *  The form here is acting entirely as a Ajax responder for a jqGrid
+ *  object on the display page. Documentation for jqGrid can be found
+ *  at: http://www.trirand.com/jqgridwiki/doku.php?id=wiki:options
+ *
+ *  @author Grant Tegtmeier <Grant@Tegt.com>
+ *  @package hofforg
+ *  @copyright no
+ */
+
+/** Safely fetch a $_GET value in strict form
+ * 
+ *  Provides a quick central check that GET parameters are defined
+ *  avoiding strict mode issues and unexpected coersions.
+ * 
+ * @param string $name the key value in the $_GET array.
+ * @param mixed $default the value used if key is absent.
+ */
+function defGet($name, $default='') {
+    if (! array_key_exists($name, $_GET))
+        return $default;
+    return $_GET[$name];
+}
+
+/** jqGrid Error msg
+ *
+ *  This is a last ditch error display. It simply passes the message
+ *  out in the xhr stream, which will cause the json parser in jqGrid
+ *  to fail. A JS function defined as the loadError option in
+ *  index.html's jqGrid parms pops an alert for the user. In a full
+ *  application the function defined to redirect to another page or do
+ *  something more helpful.
+ */
+function errOut($msg) {
+    echo 'ERROR: '.$msg;
+    exit (0);
+}
+error_reporting(E_ERROR | E_PARSE);
+
+/** Quckie sql error reflector */
+function errCheck($result) {
+    global $database;
+    if (! $result) 
+        errOut('SQL error: '.$database->error);
+}
+
+/* This is the only php file using the database so inline for
+ * simplicity http://us1.php.net/manual/en/mysqli.construct.php
+ */
+$database = new mysqli('db.tegt.com', 'hasoffers', 'hasoff4me', 'hasoffers');
+if ($database->connect_errno)
+    errOut('Database connection error '.$database->connect_errno.': '.$database->connect_error);
+
+// Obtain and condition GET parameters from jsGrid call
+$pageNum = defGet('page', 1);
+$pageRows = defGet('rows', 100);
+$sortBy = defGet('sidx', 'enum');
+$sortOrder = defGet('sord');
+if ($sortOrder <> 'desc') 
+    $sortOrder = 'asc'; // only two choices
+
+// Quick query for total number of employees
+$result = $database->query('SELECT COUNT(*) AS count FROM employees');
+errCheck($result);
+$totalRows = $result->fetch_object()->count;
+
+// Use total to calc pages and condition current page num
+$totalPages = ceil($totalRows/$pageRows);
+$pageNum = min($totalPages, max(1, $pageNum)); // clamp to range 1..total
+
+/** A query to find them all. The major speed advantage here is the
+ *  use of a complete paths table also called a "closure" table. See
+ *  smonkey.sql for the INSERT trigger that maintains this table.
+ *
+ *  The assignment here was to work from an preset table so the paths
+ *  table is only built once and only the insert trigger has been
+ *  tested.
+ *
+ *  A paths row has only three numbers. The boss id, the employee id
+ *  and the distance between them. BUT it has a row for every path in
+ *  the tree including the zero length self reference path.
+ *
+ *  The first join adds the boss' name to the row.
+ *
+ *  The second join looks up the distance to the CEO by directly
+ *  locating the path from the CEO to that employee.
+ *
+ *  The third join simply adds a row for every path where the employee
+ *  is at the top. The group by then counts the number of people that
+ *  were joined to him at any depth. (Less 1 for the path to himself.)
+ *
+ *  Paging and sorting are altered by using the four variables
+ *  calculated from the employee table size and the GET parameters set
+ *  by the jqGrid scripts.
+ */
+$query = '
+SELECT emp.id AS enum, emp.name AS ename, boss.name AS bname, 
+    span.distance AS dist, COUNT(*)-1 AS subs
+    FROM employees AS emp
+       JOIN employees AS boss ON emp.bossId=boss.id
+       JOIN empPaths AS span ON span.bossId=1 AND span.empId=emp.id
+       JOIN empPaths AS subs ON emp.id=subs.bossId
+       GROUP BY enum 
+       ORDER BY '.$sortBy.' '.$sortOrder
+    .' LIMIT '.$pageRows.' OFFSET '.$pageRows*($pageNum-1);
+
+$result = $database->query($query);
+errCheck($result);
+
+/** Construct the reply object. The AJAX requester is expecting a
+ *  structured response to set the new page values and fill the table
+ *  cells. By mimicing the structure with PHP objects and arrays the
+ *  reply can be directly encoded into json with a single call.
+ */
+$reply->page = $pageNum;
+$reply->total = $totalPages;
+$reply->records = $totalRows;
+
+for ($i = 0; $ro = $result->fetch_object(); $i++) {
+    $reply->rows[$i]['id'] = $ro->enum;
+    $reply->rows[$i]['cell'] = 
+        array ($ro->enum, $ro->ename, $ro->bname, $ro->dist, $ro->subs);
+}
+
+// Encode it in json and send it out.
+echo json_encode($reply);
+?>
---
+++ smonkey.sql
@@ -0,0 +1,73 @@
+/** This file can be fed directly to a mySql command to create the
+ * empPaths table.
+ * 
+ *  @author Grant Tegtmeier <Grant@Tegt.com>
+ *  @package hofforg
+ *  @copyright no
+ */
+
+/** Build a paths (closure) table for an existing employees table.
+ *  There is a row for every path from every superior to all
+ *  subordinates. It is much more efficient for SQL to maintain these
+ *  rows than the "closure strings" often used. As a matter of fact
+ *  it's DirtSimple. Thanks to PJ Eby there's a fine little walk
+ *  through on this at:
+ *  http://dirtsimple.org/2010/11/simplest-way-to-do-tree-based-queries.html
+ *
+ *  So first lets create the paths table for this data.
+ */
+
+DROP TABLE IF EXISTS empPaths;
+CREATE TABLE `empPaths` (
+  `bossId` int(11) NOT NULL COMMENT 'Employee table Boss id',
+  `empId` int(11) NOT NULL COMMENT 'Employee table id',
+  `distance` int(11) NOT NULL COMMENT 'distance between them',
+  KEY `empId` (`empId`),
+  KEY `bossId` (`bossId`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+/** Since employee is already filled make an empty copy of the
+ * structure.
+ */
+
+DROP TABLE IF EXISTS empDone;
+CREATE TABLE empDone LIKE employees;
+
+/** In a more dynamic example this INSERT trigger would be added to
+ *  the employee table right after it was created. That trigger would
+ *  then maintain the empPaths table as it was populated (or
+ *  restored). Here we simply attach it to a clone of the employee
+ *  structure.
+ *
+ *  It's the INSERT...SELECT cross-product that's producing all of the
+ *  rows. But they're little and it's really quick.
+ */
+
+DROP TRIGGER IF EXISTS insEmp;
+delimiter ;;
+CREATE TRIGGER insEmp AFTER INSERT ON empDone
+       FOR EACH row BEGIN
+           INSERT INTO empPaths (bossId, empId, distance)
+                  VALUES (NEW.id, NEW.id, 0);
+           IF NEW.bossId <> NEW.id THEN /* skip for the CEO */
+              INSERT INTO empPaths (bossId, empId, distance)
+                     SELECT b.bossId, e.empId, b.distance + e.distance + 1
+                            FROM empPaths AS b, empPaths AS e
+                            WHERE b.empId=NEW.bossId AND e.bossId=NEW.id;
+           END IF;
+       END;
+;;
+delimiter ;
+
+/** Now run that trigger for every row in the employee table. */
+
+INSERT INTO empDone SELECT * FROM employees;
+
+/** Since it's a clone we'll just use the empPaths table with the
+ *  original employees table. So let's drop this one. N.B. in the
+ *  mySql 5.1 I'm running there are problems if this is done with a
+ *  TEMPORARY table. Seems not to like cleaning up a TEMPORARY with a
+ *  trigger attached. So we'll just do it by hand.
+ */
+
+DROP TABLE empDone;
